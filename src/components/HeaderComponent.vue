92e1ae50084428643e3a505c39c7169044b61ef0
arcteta <ridhoadha117@gmail.com>
2024-02-26 18:34:30 +0700

    versi 1.0

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,21 +1,25 @@
 <script setup>
+import { isVNode } from 'vue';
 import RestaurantLogo from '../components/RestaurantLogo.vue'
 </script>
 <template>
-    <RestaurantLogo />
-    <nav class="flex justify-items-end">
-        <ul>
-            <li>
-                <a href="#"> Home </a>
-            </li>
-            <li>
-                <a href="#"> Menu </a>
-            </li>
-            <li>
-                <button> book now </button>
-            </li>
-        </ul>
-    </nav>
+    <div class="flex justify-items-end justify-between px-5 py-2">
+        <RestaurantLogo />
+        <nav>
+            <ul class="flex justify-items-end ">
+                <li>
+                    <a class="text-2xl p-2   bg-sky-200 mr-12" href="#"> Home </a>
+                </li>
+                <li>
+                    <a class="text-2xl p-2  bg-sky-200 mr-12" href="#"> Menu </a>
+                </li>
+                <li>
+                    <button class="text-2xl m-3 p-3 bg-firstContent text-fourthContent rounded-sm text-center "> book now
+                    </button>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </template>
 
 <style scoped></style>

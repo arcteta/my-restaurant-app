659a0d1d4e42b69538d22647aeff3461715453b1
arcteta <ridhoadha117@gmail.com>
2024-02-28 11:08:45 +0700

    ver 1.0

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,24 +1,30 @@
 <script setup>
-import { isVNode } from 'vue';
 import RestaurantLogo from '../components/RestaurantLogo.vue'
 </script>
 <template>
-    <div class="flex justify-items-end justify-between px-5 py-2">
+    <div class="flex justify-items-end justify-between flex-row m-5">
         <RestaurantLogo />
-        <nav>
-            <ul class="flex justify-items-end ">
-                <li>
-                    <a class="text-2xl p-2   bg-sky-200 mr-12" href="#"> Home </a>
+        <nav class=" text-center">
+            <ul class="flex justify-items-end justify-evenly">
+                <li class="mt-2">
+                    <a class="text-2xl bg-firstContent p-3 pt-1 text-fourthContent mr-12  shadow-lg border border-slate-800 border-r-4 border-b-4"
+                        href="#"> Home </a>
+                </li>
+                <li class="mt-2">
+
+                    <a class="text-2xl bg-firstContent p-3 pt-1 text-fourthContent mr-12 border border-slate-800 border-r-4 border-b-4"
+                        href=""> Menu </a>
+
                 </li>
                 <li>
-                    <a class="text-2xl p-2  bg-sky-200 mr-12" href="#"> Menu </a>
-                </li>
-                <li>
-                    <button class="text-2xl m-3 p-3 bg-firstContent text-fourthContent rounded-sm text-center "> book now
+                    <button
+                        class="text-2xl p-3 bg-firstContent text-fourthContent rounded-sm text-center shadow-xl border border-slate-800 border-r-4 border-b-4">
+                        book now
                     </button>
                 </li>
             </ul>
         </nav>
+
     </div>
 </template>
 
---
+++ src/views/HomeScreenView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import HeaderComponent from '../components/HeaderComponent.vue'
+</script>
+
+<template>
+  <HeaderComponent />
+
+  <div class="w-screen h-screen bg-firstContent img-bg-content" >
+    <div class="w-screen h-sceen bg-gray-950 bg-transparent"></div>
+    <div class="container flex pt-3 justify-evenly justify-items-center">
+      <div>
+        <img class="shadow-xl shadow-black " width="720" height="480" src="../assets/img/restaurant-2.jpg"
+          alt="img-error-load">
+      </div>
+
+      <div class="bg-firstContent shadow-xl text-center flex justify-center">
+        <p class="">"makanan enak itu nikmat ketika dihidangkan saat panas"</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.img-bg-content {
+  background-image: url('../assets/img/restaurant-1.jpg');
+}
+</style>
